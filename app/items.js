64eb55c7f7792bd4dcb13b1ec2e0f28624265151
Nikhil Gupta <me@nikhgupta.com>
2014-05-08 01:21:44 +0530

    updated code to support in-house proxy renewal

--- app/items.js
+++ app/items.js
@@ -3,17 +3,17 @@ var app = express();
 var port = process.env.PORT || 3001
 var events = require('events');
 var emitter = new events.EventEmitter;
-var http = require('http'); 
+var http = require('http');
 var Requester = require('requester'),
     requester = new Requester({debug: 1});
 app.get('/', function(req, res) {
-	var bucket = [];
-	var targets = require('url').parse(req.url);
-	var auth = require('url').parse(req.url).auth;
-	var hash = require('url').parse(req.url).hash;
-	res.send('Query: ' + targets.query + '\nhttps: ' + hash + '\n\nFull Text\n' + targets);
+  var bucket = [];
+  var targets = require('url').parse(req.url);
+  var auth = require('url').parse(req.url).auth;
+  var hash = require('url').parse(req.url).hash;
+  res.send('Query: ' + targets.query + '\nhttps: ' + hash + '\n\nFull Text\n' + targets);
 
 });
- 
+
 app.listen(port);
 console.log('BOUND:' + port);
--- app/web.js
+++ app/web.js
@@ -1,89 +1,93 @@
-/*/ 
-Cloak
-patrick@noodle.org, May 2013
-
-TODO
-1. Add Redirect to refresh page.
 /*/
+  Cloak
+  patrick@noodle.org, May 2013
+
+  TODO
+  1. Add Redirect to refresh page.
+/*/
+
+var d = new Date()
+process.env['REFRESHED'] = d.getTime();
+console.log("Refreshed at: " + process.env['REFRESHED']);
+var fs = require('fs');
 
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3001
 var events = require('events');
 var emitter = new events.EventEmitter;
-var http = require('http'); 
+var http = require('http');
 var Requester = require('requester'),
-    requester = new Requester({debug: 1});
+requester = new Requester({debug: 1});
+
 app.get('/', function(req, res) {
-	try { 
-		var bucket = [];
-		var targets = require('url').parse(req.url);
-			emitter.getTarget = function(url) { 
-			var self = this;
-			console.log(url); 
-			var r = requester.get(url, function (body) {
-    			self.emit('heard', body);  
-			});
-		}
-		emitter.once('heard', function(body) { 
-			res.send(body); 
-		}); 
-		emitter.getTarget(targets.query);
-	} catch (err) { 
-		res.send('CLOAK ERROR: Please enter a target url.' + err); 
-	} 
+  try {
+    var bucket = [];
+    var targets = require('url').parse(req.url);
+    emitter.getTarget = function(url) {
+      var self = this;
+      console.log(url);
+      var r = requester.get(url, function (body) {
+        self.emit('heard', body);
+      });
+    }
+    emitter.once('heard', function(body) {
+      res.send(body);
+    });
+    emitter.getTarget(targets.query);
+  } catch (err) {
+    res.send('CLOAK ERROR: Please enter a target url.<br/>' + err);
+  }
 
 });
+
+app.get('/last-refreshed', function(req, res){
+  res.send(process.env['REFRESHED']);
+})
+
+app.get('/ip', function(req, res) {
+  var targets = "http://ifconfig.me/ip";
+  emitter.getIP = function(url) {
+    var self = this;
+    console.log(url);
+    var r = requester.get(url, function (body) {
+      self.emit('ip', body);
+    });
+  }
+  emitter.once('ip', function(body) {
+    res.send(body);
+  });
+  emitter.getIP(targets);
+})
+
+app.get('/refresh-after', function(req, res){
+  res.send((parseInt(process.env['REFRESHED']) + 11 * 60 * 1000 + 1000).toString());
+})
 
 app.get('/refresh', function(req, res) {
-	try { 
-		var bucket = [];
-		var targets = "http://cloak.routedirector.com:7473/refresh";
-			emitter.getIP = function(url) { 
-			var self = this;
-			var refreshing_sprite = "CLOAK: Boot signal sent! Please wait 15 seconds before running your process.";
-			console.log(url); 
-			var r = requester.get(url, function (body) {
-    			self.emit('refresh', refreshing_sprite);  
-			});
-		}
-		emitter.once('refresh', function(body) { 
-			res.send(body); 
-		}); 
-		emitter.getIP(targets);
-	} catch (err) { 
-		res.send('CLOAK ERROR: Please enter a target url.' + err); 
-	} 
+  var bucket = [];
+  var self = this;
+  var d = new Date()
+  if (d.getTime() - process.env['REFRESHED'] > 11*60*1000 ) {
+    var refreshing_sprite = "CLOAK ["+d.getTime()+"]: Boot signal sent! Proxy IP address will be renewed, now.";
+    res.send(refreshing_sprite);
 
+    process.env['REFRESHED'] = d.getTime();
+    console.log("FS error is intentional, and intended to restart the dynos.");
+    console.log("Refreshed at: " + process.env['REFRESHED']);
+    fs.readFile('somefile.txt', function (err, data) {
+      if (err) throw err;
+    });
+  } else {
+    var refreshing_sprite = "CLOAK ["+d.getTime()+"]: IP refresh must be done after 10-12 minutes, at the earliest.";
+    res.send(refreshing_sprite);
+    console.log("Refreshed at: " + process.env['REFRESHED']);
+  }
 });
 
-app.get('/cluster', function(req, res) {
-	try { 
-		var bucket = [];
-		var targets = "http://cloak.routedirector.com:7473/cluster";
-			emitter.getIP = function(url) { 
-			var self = this;
-			var refreshing_sprite = "CLOAK: A cluster of 100 workers has been deployed for 3 hours. Please wait 2 minutes before running your process.";
-			console.log(url); 
-			var r = requester.get(url, function (body) {
-    			self.emit('refresh', refreshing_sprite);  
-			});
-		}
-		emitter.once('refresh', function(body) { 
-			res.send(body); 
-		}); 
-		emitter.getIP(targets);
-	} catch (err) { 
-		res.send('CLOAK ERROR: Please enter a target url.' + err); 
-	} 
-
-});
- 
 app.get('/help', function(req, res) {
-	res.send('CLOAK: Please create your request by appending the target domain to the cloak address, ex: "cloak.herokuapp.com/?http://www.google.com"' ); 
-}); 
+  res.send('CLOAK: Please create your request by appending the target domain to the cloak address, ex: "cloak.herokuapp.com/?http://www.google.com"' );
+});
 
 app.listen(port);
 console.log('BOUND:' + port);
-
-
